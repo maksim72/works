3c9b435e037a3a7189c0341720b11283b7f145b2
maksim72 <vozhakov.1997@gmail.com>
2022-05-04 10:56:40 +1000

    Homework for lesson 5 fixed

--- src/main/java/lesson5/Employee.java
+++ src/main/java/lesson5/Employee.java
@@ -1,12 +1,12 @@
 package lesson5;
 
 public class Employee {
-    String fcs;
-    String post;
-    String email;
-    long number;
-    int salary;
-    int age;
+    private String fcs;
+    private String post;
+    private String email;
+    private long number;
+    private int salary;
+    private int age;
 
     public Employee (String fcs, String post, String email, long number, int salary, int age) {
         this.fcs = fcs;
@@ -18,7 +18,55 @@ public class Employee {
 
     }
 
+    public String getFcs() {
+        return fcs;
+    }
 
+    public void setFcs(String fcs) {
+        this.fcs = fcs;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getNumber() {
+        return number;
+    }
+
+    public void setNumber(long number) {
+        this.number = number;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 0 && age < 150) {
+            this.age = age;
+        } else System.out.println("Введите корректное значение возраста");
+    }
 
     @Override
     public String toString() {
